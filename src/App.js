6cd6f2f80ee60219c77fc0ac31c2bf93414a9a63
nehaannvinu <nehaannvinu2607@gmail.com>
2022-02-11 15:36:45 +0530

    Added initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [tokenName, setTokenName] = useState("");
+  const [tokenSymbol, setTokenSymbol] = useState("")
+  const [tokens, setTokens] = useState([{
+    name: "",
+    symbol: ""
+  }]);
+
+  let newToken = "";
+  let newSymbol = "";
+
+  function handleTokenName(event) {
+    newToken = event.target.value;
+    setTokenName(newToken);
+  }
+
+  function handleTokenSymbol(event) {
+    newSymbol = event.target.value;
+    setTokenSymbol(newSymbol);
+  }
+
+  function handleClick() {
+
+    setTokens((prevItems) => {
+      return {
+        ...prevItems,
+        name: tokenName,
+        symbol: tokenSymbol
+      };
+    });
+    console.log(tokens)
+    setTokenName("");
+    setTokenSymbol("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h3>Card Catalogue</h3>
+      </div>
+      <div>
+        <input name="tokenname" onChange={handleTokenName} type="text" value={tokenName} />
+        <input name="tokensymbol" onChange={handleTokenSymbol} type="text" value={tokenSymbol} />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      {/* <div class="card">
+        <div class="card-body">
+          <ul>
+            {tokens.map((token) => {
+              return <p>{token}</p>;
+            })}
+          </ul>
+        </div>
+      </div> */}
     </div>
   );
 }
