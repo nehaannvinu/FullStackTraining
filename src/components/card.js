2d44f0a14b96c27fe3ee07f869f333cc6d46b4cd
nehaannvinu <nehaannvinu2607@gmail.com>
2022-02-14 13:42:27 +0530

    Added card delete button

--- src/components/card.js
+++ src/components/card.js
@@ -7,10 +7,8 @@ const Card = ({ name, symbol, id, handleDelete}) => {
             <div className="card">
                 <div className="card-body">
                     <button className="card-button" onClick={()=>handleDelete(id)}>
-                        <p>Id: {id}</p>
                         <p className="title">Token name: {name}</p>
                         <p className="title">Symbol : {symbol}</p>
-                        {/* <button onClick={()=>handleDelete(id)}>DELETE</button> */}
                     </button>
                 </div>
             </div>
